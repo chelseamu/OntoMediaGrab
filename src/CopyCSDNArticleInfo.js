22396f24f681259802730bdc09cba237c8c0040f
Edi Wang <Edi.Wang@outlook.com>
2019-05-27 13:34:33 +0800

    Add Copy CSDN js

---
+++ src/CopyCSDNArticleInfo.js
@@ -0,0 +1,56 @@
+function getCSDNPostInfo() {
+    var list = $(".tab_page_list");
+    var result = [];
+    list.each(function (i) {
+        var pubDateElement = $(this).find(".tab_page_b_r");
+        var pubDate = pubDateElement.text().trim();
+        var linkElement = $(this).find("h3").find("a");
+        var cln = linkElement.clone().children().remove().end();
+        var readCountElement = $(this).find('em');
+        var readCount = readCountElement.text().trim();
+
+        var item = {
+            pubDate: pubDate,
+            title: cln.text().trim(),
+            link: cln.attr("href"),
+            readCount: readCount
+        };
+        result.push(item);
+    });
+    return result;
+}
+
+javascript: (function () {
+    var list = $(".tab_page_list");
+    var result = [];
+    list.each(function (i) {
+        var pubDateElement = $(this).find(".tab_page_b_r");
+        var pubDate = pubDateElement.text().trim();
+        var linkElement = $(this).find("h3").find("a");
+        var cln = linkElement.clone().children().remove().end();
+        var readCountElement = $(this).find('em');
+        var readCount = readCountElement.text().trim();
+
+        var item = {
+            pubDate: pubDate,
+            title: cln.text().trim(),
+            link: cln.attr("href"),
+            readCount: readCount
+        };
+        result.push(item);
+    });
+    var resultText = "";
+    $.each(result, function (i) {
+        resultText += result[i].pubDate + "\t";
+        resultText += result[i].title + "\t";
+        resultText += result[i].link + "\t";
+        resultText += result[i].readCount + "\r";
+    });
+
+    const el = document.createElement('textarea');
+    el.value = resultText;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+})();
