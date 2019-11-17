a255157bfd8c40b4ddf0d5c586f6b3508bd3ba8d
Edi Wang <Edi.Wang@outlook.com>
2019-11-17 22:41:54 +0800

    Update huoxing for new URL #2

--- src/huoxing.js
+++ src/huoxing.js
@@ -1,6 +1,44 @@
 // https://www.huoxing24.com/userCenter/6f21ae1a698e4d0d988297267ee58312
 
 javascript: (function() {
+    var list = $(".news-item");
+    var result = [];
+    list.each(function(i) {
+        var pubDateElement = $(this).find("span.date");
+        var pubDate = pubDateElement.text().trim();
+
+        var readCountElement = $(this).find('.hotNum');
+        var readCount = readCountElement.text().trim().replace("热度 ", "");
+
+        var linkElement = $(this).find("a");
+
+        var item = {
+            pubDate: pubDate,
+            title: $(this).find(".item-title").text().trim(),
+            link: linkElement.attr("href"),
+            readCount: readCount
+        };
+        result.push(item);
+
+        var resultText = "";
+        $.each(result, function(i) {
+            resultText += result[i].pubDate + "\t";
+            resultText += result[i].title + "\t";
+            resultText += result[i].link + "\t";
+            resultText += result[i].readCount + "\r";
+        });
+
+        const el = document.createElement('textarea');
+        el.value = resultText;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+    });
+})();
+
+// Obsolete via #2
+javascript: (function() {
     var list = $(".activity-list");
     var result = [];
     list.each(function(i) {
